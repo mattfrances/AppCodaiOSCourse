88830d7123cbe5ffed757780517b9cfa3ea23808
mattfrances <matfran23@gmail.com>
2019-03-20 11:44:43 -0400

    Updated HelloWorld and Exercise #1 Projects

--- Chapter3/HelloWorld/HelloWorld/ViewController.swift
+++ Chapter3/HelloWorld/HelloWorld/ViewController.swift
@@ -1,58 +1,15 @@
-//
-//  ViewController.swift
-//  HelloWorld
-//
-//  Created by Matthew Frances on 2019-03-19.
-//  Copyright © 2019 Matthew Frances. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-//    @IBAction func showMessage(sender: UIButton) {
-//        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
-//        alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
-//        present(alertController, animated: true, completion: nil)
-//    }
-    
     @IBAction func showMessage(sender: UIButton) {
-        
-        // Initialize the dictionary for the emoji icons
-        // If you forgot how to do it, refer to the previous chapter
-        // Fill in the code below
-        var emojiDict: [String: String] = ["👾": "Purple Martian",
-                                           "👻":"Ghost",
-                                           "🤓":"Nerd",
-                                           "🤖":"Robot"]
-        
-        
-        // The sender is the button that is tapped by the user.
-        // Here we store the sender in the selectedButton constant
-        let selectedButton = sender
-        
-        // Get the emoji from the title label of the selected button
-        if let wordToLookup = selectedButton.titleLabel?.text {
-            
-            // Get the meaning of the emoji from the dictionary
-            // Fill in the code below
-            let meaning = emojiDict[wordToLookup]
-            
-            
-            
-            // Change the line below to display the meaning of the emoji instead of Hello World
-            let alertController = UIAlertController(title: "Meaning", message: meaning, preferredStyle: UIAlertController.Style.alert)
-            
-            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-            present(alertController, animated: true, completion: nil)
-        }
-        
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
     }
-
 }
-
