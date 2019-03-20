6f93378d9ca5178d3258ec93479fd3509c53a4cb
mattfrances <matfran23@gmail.com>
2019-03-19 22:53:28 -0400

    First Commit

---
+++ Chapter2/ConstantsAndVariables.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let constant: String = "Constant"
+
+var emojiDict: [String: String] = ["👻": "Ghost",
+                                   "🤧": "Sneeze"]
+
+var wordToLookUp = "👻"
+emojiDict[wordToLookUp]
