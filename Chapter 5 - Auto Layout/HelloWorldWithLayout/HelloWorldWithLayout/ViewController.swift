bbdd1ede13ab870f0adfcd5695b67fe4630535ef
mattfrances <matfran23@gmail.com>
2019-03-23 12:01:24 -0400

    Completed Chapter 5

---
+++ Chapter 5 - Auto Layout/HelloWorldWithLayout/HelloWorldWithLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldWithLayout
+//
+//  Created by Matthew Frances on 2019-03-23.
+//  Copyright © 2019 Matthew Frances. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
+
